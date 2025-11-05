e3278d5b3a19dfa6062df0f9343683dbc77e4d66
shinasahammedk <shinasahammed93@gmail.com>
2025-11-05 10:50:51 +0530

    requirment changed

--- src/libmgmt/libmgmt/settings.py
+++ src/libmgmt/libmgmt/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get(
             "DATABASE_URL",
-            "postgresql://postgres:[YOUR_PASSWORD]@db.omhdnpxxizaaarskczkf.supabase.co:5432/postgres"
+            "postgresql://postgres:[shinas@6363]@db.omhdnpxxizaaarskczkf.supabase.co:5432/postgres"
         ),
         conn_max_age=600,
         ssl_require=True,
